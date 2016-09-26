3d29a1ca1f8fbeab5992d4fcfb730f6767025404
Vivian Wang <oxygenjing830@gmail.com>
2016-09-26 17:33:18 +0800

    增加学习经历项目

--- src/component/Resume/main.jsx
+++ src/component/Resume/main.jsx
@@ -11,10 +11,10 @@ var Resume = React.createClass({
                 </header>
                 <BasicInfo basicinfo={this.props.resume.basicinfo}/>
                 <Social social={this.props.resume.social}/>
+				<ExperienceList type="学习经历" experiences={this.props.resume.StudyExperience}/>
                 <ExperienceList type="获奖经历" experiences={this.props.resume.Awards}/>
                 <ExperienceList type="社团/组织" experiences={this.props.resume.socialExperiences}/>
                 <ExperienceList type="实习经历" experiences={this.props.resume.InternExperiences}/>
-                <ExperienceList type="学习经历" experiences={this.props.resume.ProjectExperience}/>
             </div>
         )
     }
